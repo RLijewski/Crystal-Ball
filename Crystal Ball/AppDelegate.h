753a538cf45872cd9da2a403cfc310449485b801
Ryan Lijewski <pxihcky35@Ryans-MacBook-Pro.local>
2013-08-15 18:22:35 -0500

    Initial Commit

---
+++ Crystal Ball/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Crystal Ball
+//
+//  Created by Ryan Lijewski on 8/15/13.
+//  Copyright (c) 2013 Ryan Lijewski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Crystal Ball/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Crystal Ball
+//
+//  Created by Ryan Lijewski on 8/15/13.
+//  Copyright (c) 2013 Ryan Lijewski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
